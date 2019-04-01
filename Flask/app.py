2687860dbb2a8a365cd87f55b781f3196cdeb3b0
Will D <willdoucet@gmail.com>
2019-03-31 22:53:04 -0700

    first flask commit, prediction model working will do the rest tomorrow before class

---
+++ Flask/app.py
@@ -0,0 +1,164 @@
+from flask import Flask, render_template, url_for, flash, redirect, jsonify, Response, send_file
+from flask_pymongo import PyMongo
+import json
+from bson import json_util
+from bson.json_util import loads, dumps, CANONICAL_JSON_OPTIONS 
+import datetime as dt
+import sklearn
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/energyDB'
+mongo = PyMongo(app)
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('index.html', title="HOME PAGE JUMBO TEXT HERE")
+
+@app.route("/api/forecast/<date>")
+def forecast(date):
+
+    def date_to_dotw(datetime):
+        switcher = {
+            0: 1,
+            1: 2,
+            2: 3,
+            3: 4,
+            4: 5,
+            5: 6,
+            6: 0
+        }
+        return switcher.get(datetime, "nothing")
+
+    def calc_load(inputs):
+        import_model = open("../Random_Forest_Model.pkl", "rb")
+        model = pickle.load(import_model)
+        time_list = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
+                    "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
+        load_list = []
+        for input in inputs:
+
+            prediction = model.predict([input])
+            print(prediction)
+            load_list.append(prediction[0])
+
+        print(load_list)
+        load_dict = []
+        for x in range(0, len(load_list)): 
+            temp_dict = {'TIME': time_list[x], 'LOAD': load_list[x]}
+            load_dict.append(temp_dict)
+
+        print(load_dict)
+
+        return load_dict
+
+
+
+
+
+
+    forecast_data = mongo.db.forecastSDGE.find({'DATE': date}, {'_id': False})
+
+    forecast_date_string = forecast_data[0]['DATE']
+    print('date is ' + forecast_date_string)
+
+    forecast_date = dt.datetime.strptime(forecast_date_string, '%Y-%m-%d').date().weekday()
+    forecast_dotw = date_to_dotw(forecast_date)
+
+    print(forecast_date)
+    print(date_to_dotw(forecast_date))
+
+    forecast_list_master = []
+    for record in forecast_data:
+        forecast_list = []
+        forecast_list.append(record['TMP'])
+        forecast_list.append(record['DPT'])
+        forecast_list.append(record['HUM'])
+        forecast_list.append(record['CC'])
+        forecast_list.append(0)
+
+        if forecast_dotw == 0:
+            forecast_list.append(1)
+        else:
+            forecast_list.append(0)
+
+        if forecast_dotw == 1:
+            forecast_list.append(1)
+        else:
+            forecast_list.append(0)
+
+        if forecast_dotw == 2:
+            forecast_list.append(1)
+        else:
+            forecast_list.append(0)
+
+        if forecast_dotw == 3:
+            forecast_list.append(1)
+        else:
+            forecast_list.append(0)
+
+        if forecast_dotw == 4:
+            forecast_list.append(1)
+        else:
+            forecast_list.append(0)
+
+        if forecast_dotw == 5:
+            forecast_list.append(1)
+        else:
+            forecast_list.append(0)
+
+        if forecast_dotw == 6:
+            forecast_list.append(1)
+        else:
+            forecast_list.append(0)
+
+        forecast_list.append(forecast_dotw)
+        forecast_list.append(int(record['HOUR']))
+
+        forecast_list_master.append(forecast_list)
+    print(forecast_list_master)
+    print(forecast_list_master[0])
+    prediction_list = calc_load(forecast_list_master)
+
+
+
+
+
+    return jsonify(projection_inputs = prediction_list)
+
+@app.route("/api/forecast_dates")
+def forecast_dates():
+
+    forecast_data = mongo.db.forecastSDGEdates.find({}, {'_id': False})
+    forecast_dates = []
+    for date in forecast_data:
+
+        forecast_dates.append({'DATE': date['DATE']})
+    return jsonify(forecast_dates)
+
+    # for record in forecast_data:
+    #     forecast_dates.append(record['DATE'])
+
+    # print(forecast_dates)
+
+@app.route("/model")
+def model():
+    return render_template('model.html')
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Flask/static/model.js
@@ -0,0 +1,176 @@
+function init() {
+    d3.json("/api/forecast_dates").then((data) => {
+
+        console.log(data)
+        date_dropdown = d3.select("#date-selection")   
+        data.forEach((date) => {
+            date_dropdown.append("option")
+                .attr("value", date['DATE'])
+                .text(date['DATE'])
+            console.log(date['DATE'])
+        })
+
+    })
+
+    var z = d3.scaleLinear()
+        .range(["#ba721f", "#9b150c"])
+
+
+    var $container = $('#chart-area')
+    var svg = d3.select("#chart-area").append('svg');        
+        // xAxis = svg.select("#x-axis"),
+        // yAxis = svg.select("#y-axis");
+
+    var margin = {
+            top: 40,
+            right: 40,
+            bottom: 40,
+            left: 40
+        };
+
+    svgWidth = $container.width();
+    svgHeight = 600;
+    chartWidth = svgWidth - margin.left - margin.right; 
+    chartHeight = 600 - margin.top - margin.bottom;
+
+    svg 
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+
+        .style("border", "1px solid")
+
+    var chartGroup = svg.append("g")
+        .attr("id", "chart-group")
+        .attr("transform", "translate(40,40)")
+
+    var xAxis = chartGroup.append("g")
+                    .attr("id", "x-axis")
+                    .attr("transform", `translate(0, ${chartHeight})`)
+
+    var yAxis = chartGroup.append("g")
+                .attr("id", "y-axis")
+
+    filter_button = d3.select("#filter-btn")
+
+    filter_button.on("click", function() {
+        d3.event.preventDefault();
+        var inputElement = d3.select("#date-selection").node().value
+        console.log(inputElement)
+
+        d3.json("/api/forecast/" + inputElement).then((projection_list) => {
+            console.log(projection_list)
+            projections = projection_list['projection_inputs']
+            console.log(projections)
+
+            chartGroup.data(projection_list)
+            var maxY = d3.max(projections, function(d) {return d.LOAD})
+            var minY = d3.min(projections, function(d) {return d.LOAD})
+            console.log("max: " + maxY)
+
+            z.domain([minY, maxY])
+
+            var timemap = projections.map(d => {return d['TIME']})
+            console.log(timemap)
+
+            yScale = d3.scaleLinear()
+                .domain([0, maxY])
+                .range([chartHeight, 0])
+            console.log(yScale)
+
+            xScale = d3.scaleBand()
+                .domain(projections.map(d => {return d['TIME']}))
+                .range([0, chartWidth])
+                .padding(.4)
+
+            var leftAxis = d3.axisLeft(yScale)
+            var botAxis = d3.axisBottom(xScale)
+
+            var chart = d3.select("#chart-group")
+            // if (d3.selectAll("#y-axis").empty()) {
+            svg.select("#y-axis")
+            .transition()
+            .duration(500)
+            .call(leftAxis);
+
+            svg.select("#x-axis")
+            .transition()
+            .duration(500)
+            .call(botAxis)
+                // .selectAll("text")
+                // .style("text-anchor", "end")
+                // .attr("font-size", "1.2em")
+                // .attr("dx", "-10px")
+                // .attr("dy", "0px")
+                // .attr("transform", "rotate(-55)")
+
+            var bars = svg.selectAll(".bar")
+
+            bars
+                .data(projections)
+                .enter().append("rect")
+                    .attr("class", "bar")
+                    .attr("x", function(d) {return xScale(d.TIME) + 40})
+                    .attr("width", xScale.bandwidth())
+                    .attr("y", chartHeight + 40)
+                    .attr("height", 0)
+                    .attr("fill", d => {return z(d.LOAD)})
+                    .merge(bars)
+                    .transition()
+                    .duration(599)
+                    .attr("y", d => {return yScale(d.LOAD) + 40})
+                    .attr("height", d => {return (chartHeight - yScale(d.LOAD))})
+                    .attr("fill", d => {return z(d.LOAD)})
+            console.log(yScale(1875))
+
+            var toolTip = d3.tip()
+                .attr("class", "tooltip")
+                .html(d => {return d.LOAD})
+
+            chartGroup.call(toolTip)
+
+            d3.selectAll("rect").on("mouseover", function(d) {
+                console.log("mouseover")
+                toolTip
+                .offset([-10,0])
+                .show(d, this)
+            }).on("mouseout", function(d) {
+                toolTip.hide(d)
+            })
+            // }else if (!d3.selectAll("#y-axis").empty()) {
+            //     d3.select("#y-axis")
+            //     .transition()
+            //     .call(leftAxis)
+
+            //     bars
+            //     .data(projections)
+            //     .enter().append("rect")
+            //         .attr("class", "bar")
+            //         .attr("width", xScale.bandwidth())
+            //         .attr("y", d => {return yScale(d.LOAD) + 40})
+            //         .attr("height", 0)
+            //         .merge(b)
+            //         .transition()
+            //         .duration(599)
+            //         .attr("height", d => {return chartHeight - yScale(d.LOAD)})
+            // }
+
+            g = chart.append("g")
+                .selectAll("g")
+                .enter().append("g")
+                .attr("transform", function(d) { return "translate(" + xScale(d['TIME']) + ",0)"; });
+
+
+            
+
+
+        })
+
+
+
+    })
+}
+
+
+
+
+init()
